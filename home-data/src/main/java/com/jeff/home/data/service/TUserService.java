af9ac48806e69a2be52d4ed616bdc6c98733c180
jeffpeng <jeffpeng@fenqile.com>
2017-03-29 19:36:25 +0800

    0329

--- home-data/src/main/java/com/jeff/home/data/service/TUserService.java
+++ home-data/src/main/java/com/jeff/home/data/service/TUserService.java
@@ -3,19 +3,10 @@ package com.jeff.home.data.service;
 import com.jeff.home.data.model.TUser;
 
 /**
- * Created by Administrator on 2017/3/20.
+ * Created by JeffPeng on 2017/3/29.
  */
 public interface TUserService {
+    public TUser selectByPrimaryKey(int id);
 
-    public int deleteByPrimaryKey(Integer id);
-
-    public int insert(TUser record);
-
-    public int insertSelective(TUser record);
-
-    public TUser selectByPrimaryKey(Integer id);
-
-    public int updateByPrimaryKeySelective(TUser record);
-
-    public int updateByPrimaryKey(TUser record);
+    public int insertSelective(TUser tUser);
 }
